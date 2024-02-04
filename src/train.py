1e2da240b80a8ad8035f03d2adb03a72342a32a9
User <trane-101@yandex.ru>
2024-02-04 12:57:39 +0400

    modified the training loop

--- src/train.py
+++ src/train.py
@@ -1,11 +1,11 @@
 import wandb
 import pytorch_lightning as pl
 
-from model import MultiTaskBertModel
-from utils import bert_config, tokenizer
-from data_loader import load_dataset
-from data_processing import tokenized_dataset
-from data_module import DataModule
+from src.model import MultiTaskBertModel
+from src.utils import bert_config, tokenizer
+from src.data_loader import load_dataset
+from src.data_processing import tokenized_dataset
+from src.data_module import DataModule
 
 wandb.login()
 
@@ -27,8 +27,9 @@ trainer = pl.Trainer(
     logger = wandb_logger,
     log_every_n_steps=40,
     max_epochs=2,
-    deterministic=True)
-# a808ea3766056b84413558f8145c7abf7c2e9e76
+    deterministic=True,
+    profiler='simple')
+
 trainer.fit(model, dm)
 
 wandb.finish()
