b7964bc34d8e40d7c86f46d4b5ec9cd93be87bcb
User <trane-101@yandex.ru>
2024-02-12 21:50:06 +0400

    updated the prediction function

--- src/inference.py
+++ src/inference.py
@@ -2,6 +2,7 @@ import torch
 import os
 import sys
 from transformers import BertTokenizerFast
+import numpy as np
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_dir)
@@ -32,7 +33,8 @@ def preprocess_input(input_data):
 
     input_ids = torch.tensor([preprocessed_input['input_ids']])
     attention_mask = torch.tensor([preprocessed_input['attention_mask']])
-    return input_ids, attention_mask
+    offset_mapping = torch.tensor(preprocessed_input['offset_mapping'])
+    return input_ids, attention_mask, offset_mapping
 
 def perform_inference(model, input_ids, attention_mask):
 
@@ -42,23 +44,42 @@ def perform_inference(model, input_ids, attention_mask):
 
     return ner_logits, intent_logits
     
-def postprocess_output(ner_logits, intent_logits):
-    pass
+def align_predictions_with_input(predictions, offset_mapping, input_text):
+    aligned_predictions = []
+    current_word_idx = 0
+
+    # Iterate through each prediction and its offset mapping
+    for prediction, (start, end) in zip(predictions, offset_mapping):
+        # Find the corresponding word in the input text
+        word = input_text[start:end]
+
+        # Check if the current word is a special token or part of padding
+        if not word.strip():
+            continue
+
+        # Assign the prediction to the word
+        aligned_predictions.append((word, prediction))
+    
+    return aligned_predictions
 
 def main():
     model_path = "C:/Users/Userpc/Desktop/model/lit-wandb/a5yyvgve/checkpoints/epoch=0-step=35.ckpt"
     model = load_model(model_path)
 
     input_data = "Set a timer for 15 minutes"
-    input_ids, attention_mask = preprocess_input(input_data)
+    input_ids, attention_mask, offset_mapping = preprocess_input(input_data)
 
     ner_logits, intent_logits = perform_inference(model, input_ids, attention_mask)
 
-    print(f"Ner logits: {ner_logits.view(-1, 9).shape}")
-    print(f"Intent logits: {intent_logits}")
+    # print(f"Ner logits: {ner_logits.view(-1, 9).shape}")
+    # print(f"Intent logits: {intent_logits}")
 
     ner_logits = torch.argmax(ner_logits.view(-1, 9), dim=1)
     intent_logits = torch.argmax(intent_logits)
+
+    print(offset_mapping)
+
+    ner_logits = align_predictions_with_input(ner_logits, offset_mapping, input_data)
 
     print(f"Ner logits: {ner_logits}")
     print(f"Intent logits: {intent_logits}")
